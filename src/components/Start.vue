5dd0839af9143e1b9fa34033eb4997a981a067da
Matthew Kindness <matt@digiwurx.com>
2019-06-19 23:46:18 -0400

    Initial Commit.

---
+++ src/components/Start.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex xs12>Start Here!</v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ tests/unit/start.spec.js
@@ -0,0 +1,12 @@
+// import { shallowMount } from '@vue/test-utils';
+// import Start from '@/components/Start.vue';
+
+// describe('Start.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message';
+//     const wrapper = shallowMount(Start, {
+//       propsData: { msg },
+//     });
+//     expect(wrapper.text()).toMatch(msg);
+//   });
+// });
