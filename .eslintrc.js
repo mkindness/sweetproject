dbce07f2aa12fea54aa9ed18a8a469245d29d9a7
Matthew Kindness <matt@digiwurx.com>
2019-06-20 00:04:42 -0400

    Update linter.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,20 @@
-
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
+z
